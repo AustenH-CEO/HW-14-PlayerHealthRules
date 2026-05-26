19ec49944d1646554c80bf709c1051372e7a2e49
MRHOM <MRHOM@LAPTOP-9RSIE69G>
2026-05-26 16:05:07 -0600

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayerFile
+{
+    public class Player
+    {
+        private int health;
+
+        public Player(int health)
+        {
+            Health = health;
+        }
+        public int Health
+        {
+            get { return health; }
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                if (value > 100)
+                {
+                    value = 100;
+                }
+                else
+                    health = value;
+
+            }
+        }
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+            if (Health == 0)
+            {
+                Console.WriteLine("Player takes " + amount + " damage.");
+                Console.WriteLine("Player Defeated");
+            }
+            else
+            {
+                Console.WriteLine("Player takes " + amount + " damage.");
+            }
+        }
+        public void Heal(int amount)
+        {
+            if (Health == 100)
+            { 
+                Console.WriteLine("Player is already healed");
+            }
+            else
+            {
+                Health += amount;
+                Console.WriteLine("Player heals " + amount);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// This program will simulate combant and healing a player character
+// Austen Hernandez
+// 5/26/26
+
+using PlayerFile;
+
+public class Program
+{
+    public static void Main()
+    {
+        Player player1 = new Player(100);
+        Console.WriteLine("Player is in combat");
+        player1.TakeDamage(50);
+        Console.WriteLine("Health: " + player1.Health);
+        player1.Heal(50);
+        Console.WriteLine("Health: " + player1.Health);
+        player1.Heal(50);
+        Console.WriteLine("Health: " + player1.Health);
+        player1.TakeDamage(200);
+        Console.WriteLine("Health: " + player1.Health);
+    }
+}
